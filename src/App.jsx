a7d3720681a6a00b695c18b6502da38981f09748
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <leg.gwenaelle@gmail.com>
2024-03-21 16:36:35 +0100

    Done day 4

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,23 @@
+import { Routes, Route } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
-import SideBar from "./components/SideBar/SideBar";
+// import SideBar from "./components/SideBar/SideBar";
 import Footer from "./components/Footer/Footer";
-import Tasks from "./components/Tasks/Tasks";
 import "./App.css";
+import NotFoundPage from "./pages/NotFoundPage";
+import AboutPage from "./pages/AboutPage";
+import DashboardPage from "./pages/DashboardPage";
 
 function App() {
   return (
     <>
       <NavBar />
-      <div className="main-container">
-        <SideBar />
+      <Routes>
+        <Route path="/" element={<DashboardPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
 
-        <Tasks />
-      </div>
+      <div className="main-container">{/* <SideBar /> */}</div>
       <Footer />
     </>
   );
---
+++ src/pages/DashboardPage.jsx
@@ -0,0 +1,13 @@
+import Tasks from "./../components/Tasks/Tasks";
+
+function DashboardPage() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+
+      <Tasks />
+    </div>
+  );
+}
+
+export default DashboardPage;
