b8efc9a70d122f703effc588da489802c8ced747
cd ~/code/$GITHUB_USERNAME/dotfiles && zsh git_setup.sh <leg.gwenaelle@gmail.com>
2024-03-27 00:48:00 +0100

    Fix form update tasks show

--- src/components/Tasks/Tasks.jsx
+++ src/components/Tasks/Tasks.jsx
@@ -53,64 +53,66 @@ function Tasks({ tasks, setTasks }) {
   return (
     <div className="list-container">
       <h2>TOTAL: {tasks.length} TASKS </h2>
-      {sortedTasks.map((task) => {
-        return (
-          <div key={task.id} className="task-container-list">
-            <>
-              <div className="task-un">
-                <div className="check-part">
-                  <p>Due: {task.dueDate}</p>
-                  <label class="checkbox-label">
-                    <input
-                      id="checkbox-st"
-                      type="checkbox"
-                      checked={task.status}
-                      onChange={() => handleCheckboxChange(task.id)}
-                    />
-                  </label>
-                </div>
-                <div className="task-ti-cat">
-                  <span>{task.title}</span>
-                  <p className="task-cat">{task.category}</p>
-                </div>
-                <p>{task.description}</p>
-                <button onClick={() => handleUpdate(task.id)}>Update</button>
-                <button onClick={() => handleDelete(task.id)}>Delete</button>
+      {sortedTasks.map((task) => (
+        <div key={task.id} className="task-container-list">
+          {editingTask === task.id ? (
+            <form onSubmit={handleSubmit} className="form-update-tasks">
+              <label>
+                Update your title :
+                <input
+                  type="text"
+                  name="title"
+                  value={updatedTask.title}
+                  onChange={handleChange}
+                  required
+                />
+              </label>
+              <label>
+                Update your description :
+                <input
+                  name="description"
+                  value={updatedTask.description}
+                  onChange={handleChange}
+                  required
+                />
+              </label>
+              <label>
+                Update your due date :
+                <input
+                  type="date"
+                  name="dueDate"
+                  value={updatedTask.dueDate}
+                  onChange={handleChange}
+                  required
+                />
+              </label>
+              <button type="submit">Save</button>
+            </form>
+          ) : (
+            <div className="task-un">
+              <div className="check-part">
+                <p>Due: {task.dueDate}</p>
+                <label className="checkbox-label">
+                  <input
+                    id="checkbox-st"
+                    type="checkbox"
+                    checked={task.status}
+                    onChange={() => handleCheckboxChange(task.id)}
+                  />
+                </label>
               </div>
-            </>
-            {editingTask === task.id && (
-              <>
-                {" "}
-                <form onSubmit={handleSubmit}>
-                  <input
-                    type="text"
-                    name="title"
-                    value={updatedTask.title}
-                    onChange={handleChange}
-                    required
-                  />
-                  <textarea
-                    name="description"
-                    value={updatedTask.description}
-                    onChange={handleChange}
-                    required
-                  ></textarea>
-                  <input
-                    type="date"
-                    name="createdAt"
-                    value={updatedTask.createdAt}
-                    onChange={handleChange}
-                    required
-                  />
-                  <button type="submit">Save</button>
-                </form>
-              </>
-            )}
-          </div>
-        );
-      })}
+              <div className="task-ti-cat">
+                <span>{task.title}</span>
+                <p className="task-cat">{task.category}</p>
+              </div>
+              <p>{task.description}</p>
+              <button onClick={() => handleUpdate(task.id)}>Update</button>
+              <button onClick={() => handleDelete(task.id)}>Delete</button>
+            </div>
+          )}
+        </div>
+      ))}
     </div>
   );
 }
-
 export default Tasks;
